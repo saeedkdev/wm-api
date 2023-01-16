316846589179ea999ccb415cbdd28f5d1add9d37
skdev <saeed.karimi@generalinternet.ca>
2023-01-16 00:49:12 -0800

    Auth added

--- server.js
+++ server.js
@@ -52,3 +52,53 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.post('/register', async (req, res) => {
+    const { username, password, email } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+        username,
+        password: hashedPassword,
+        email,
+    });
+
+    try {
+        await user.save();
+        res.send('User created');
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    console.log(email);
+    const user = await User.findOne({ email });
+    console.log(user);
+
+    if (!user) return res.send('User not found');
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) return res.send('Invalid Login Information');
+
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    if (!token) return res.send('Token not valid');
+
+    res.header('auth-token', token).send(token);
+});
+
+function validateToken(req, res, next) {
+    const authToken = req.header('auth-token');
+    const token = authToken && authToken.split(' ')[1];
+    if (!token) return res.status(401).send('Access Denied');
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(403).send('Invalid Token');
+        req.user = user;
+        next();
+    });
+}
+
+app.get('/notes', validateToken, async (req, res) => {
+    const notes = await Note.find({ user: req.user._id });
+    res.send(notes);
+});
